c22d1a2e3bbf2689b71904f4077315a560be51ba
deloris19904 <73267872+deloris19904@users.noreply.github.com>
2020-12-03 09:56:29 -0500

    reversed string code

---
+++ string.js
@@ -0,0 +1,20 @@
+ let strings="str"
+ let string = reverseString.strings();
+     return reverseString(strings);
+     
+
+
+     let strings="SuperDeeDuper"
+     let string = reverseString.strings();
+         return reverseString(strings);     
+     
+
+
+
+
+
+
+
+
+
+     
